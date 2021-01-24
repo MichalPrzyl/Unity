f01e1b0cd38ee00b7003afee6d67561deb15e518
MichalPrzyl <michal.przyl@gmail.com>
2021-01-24 21:37:03 +0100

    Dodany skrypt poruszania sie w sposob tibijski

---
+++ 2d/Tibia_movement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    
+    Vector3 newPos;
+    bool canMove = true;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       
+        if(Input.GetKey(KeyCode.D) && canMove)
+        {
+            newPos = new Vector3(transform.position.x + 1, transform.position.y, 0);
+            StartCoroutine(Move(newPos, speed));
+
+        }
+
+
+        if (Input.GetKey(KeyCode.A) && canMove)
+        {
+            newPos = new Vector3(transform.position.x - 1, transform.position.y, 0);
+            StartCoroutine(Move(newPos, speed));
+        }
+        if (Input.GetKey(KeyCode.W) && canMove)
+        {
+            newPos = new Vector3(transform.position.x, transform.position.y + 1, 0);
+            StartCoroutine(Move(newPos, speed));
+        }
+        if (Input.GetKey(KeyCode.S) && canMove)
+        {
+            newPos = new Vector3(transform.position.x, transform.position.y - 1, 0);
+            StartCoroutine(Move(newPos, speed));
+        }
+    }
+
+    IEnumerator MoveRight()
+    {
+        canMove = false;
+        transform.position = Vector3.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
+        yield return 0;
+        canMove = true;
+    }
+
+    IEnumerator Move(Vector3 destination, float speed)
+    {
+        canMove = false;
+        while(transform.position != destination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return 0;
+        }
+        canMove = true;
+    }
+
+  
+}
