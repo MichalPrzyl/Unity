d4e6427bc1055be1f1103d158253f99c05433966
MichalPrzyl <michal.przyl@gmail.com>
2021-01-24 19:11:05 +0100

    Dodany foled 3d ze skryptem poruszania sie

---
+++ 3d/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Camera cam;
+    public float mouseSensitivity = 100.0f;
+    public float jumpSpeed;
+    float timer;
+
+
+
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 velocity = move * speed * Time.deltaTime;
+        transform.Translate(velocity);
+
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKeyDown(KeyCode.Space) && timer > 1.2f)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 1, 0) * jumpSpeed;
+            timer = 0f;
+        }
+    }
+}
