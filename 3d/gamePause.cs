d266aab8ad60137f6908149bc168825778350b27
MichalPrzyl <michal.przyl@gmail.com>
2021-01-25 19:49:31 +0100

    dodany plik z obsluga panelu menu

---
+++ 3d/gamePause.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamePause : MonoBehaviour
+{
+    public GameObject panelPause;
+    bool GameisPaused = false;
+    playerMovement pm;
+
+    private void Start()
+    {
+        pm = GameObject.Find("player").GetComponent<playerMovement>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameisPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        panelPause.GetComponent<Animator>().SetTrigger("hide");
+        StartCoroutine(PanelDisable());
+        GameisPaused = false;
+        pm.canMove = true;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        panelPause.SetActive(true);
+        panelPause.GetComponent<Animator>().SetTrigger("show");
+        GameisPaused = true;
+        pm.canMove = false;
+    }
+
+    IEnumerator PanelDisable()
+    {
+        yield return new WaitForSeconds(0.12f);
+        panelPause.SetActive(false);
+    }
+
+}
